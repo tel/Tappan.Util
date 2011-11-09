74d11272e5b9fe2ae094c5f102961511b9d51876
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-11-09 15:01:45 -0500

    Added colorized clj-stacktrace.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject util "0.0.1-SNAPSHOT"
+(defproject tappan/util "0.0.1-SNAPSHOT"
   :description "Utility functions for Tappan."
   :warn-on-reflection true
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :repl-options [:init nil :caught clj-stacktrace.repl/pst+]
+  :clj-stacktrace {:test-color true})
