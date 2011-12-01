ba34f8b01ae5b8942b7a791df30bf95e67014139
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-11-30 22:02:27 -0500

    Bumped to 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tappan/util "0.0.1-SNAPSHOT"
+(defproject tappan/util "0.0.2-SNAPSHOT"
   :description "Utility functions for Tappan."
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.2.1"]]
